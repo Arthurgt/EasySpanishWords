a2bebf1f9ac64a2f0ea0c066968d78b933d50a71
Arthurgt <arturbabkiewicz@gmail.com>
2021-08-28 19:11:53 +0200

    Finished application

--- EasySpanishWords/src/app/app.component.ts
+++ EasySpanishWords/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WordsService } from './services/words.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EasySpanishWords';
+
+  constructor(private wordsService: WordsService) {}
+
+  get nouns() {
+    return this.wordsService.getNouns();
+  }
+
+  get verbs() {
+    return this.wordsService.getVerbs();
+  }
 }
